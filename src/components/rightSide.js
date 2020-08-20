c6e2dbc52a7fdfcfb4348c11fea0a1c3434e6d72
Janessa Garrow <janessa.garrow@gmail.com>
2020-08-19 21:48:48 -0600

    Create stamina rings

--- src/components/rightSide.js
+++ src/components/rightSide.js
@@ -3,13 +3,33 @@ import styled from "styled-components"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-import TextInfo from "./textInfo"
-
 const Container = styled.div`
     width: 50%;
     position: relative;
     display: flex;
     justify-content: center;
+`
+
+const StaminaRings = styled.div`
+    border: 4px solid #03ee54;
+    border-radius: 50%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 40px;
+    height: 40px;
+
+    &:after {
+        content: '';
+        position: absolute;
+        top: 1px;
+        left: 1px;
+        border: 9px solid #03ee54;
+        border-radius: 50%;
+        width: 30px;
+        height: 30px;
+        box-sizing: border-box;
+    }
 `
 
 const ImgContainer = styled.div`
@@ -41,14 +61,13 @@ const RightSide = ({ itemInFocus, prevItemInFocus, category }) => {
     return (
         <Container>
             {/* stat boost stuff */}
+            <StaminaRings />
 
             {/* Link pic */}
             <ImgContainer>
                 <StyledImg fluid={data.file.childImageSharp.fluid} alt="Link wearing beginning 'well-worn' outfit"/>
             </ImgContainer>
 
-            {/* text info box */}
-            {/* {itemInFocus && <TextInfo itemInFocus={itemInFocus} prevItemInFocus={prevItemInFocus} category={category}/>} */}
         </Container>
     )
 }
